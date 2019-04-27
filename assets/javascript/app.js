867956a9797dcf2d0eb709bad2641d71dffdc16c
Jing Xie <xiejing418@gmail.com>
2019-04-26 23:18:03 -0700

    updated timer when it hit 0 second

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,51 +11,51 @@ $(document).ready(function () {
 
 
     var triviaGame = [{
-        question: "What is the most popular fruit in the world?",
+        question: "Q: What is the most popular fruit in the world?",
         answer: ["Banana", "Apple", "Tomato", "Watermelon"],
         correct: "Tomato",
         image: "<div style=\"width:30%;height:0;padding-bottom:20%;position:relative;\"><iframe src=\"https://giphy.com/embed/1zi2SFsjUwzB0at4h1\" width=\"100%\" height=\"100%\" style=\"position:absolute\" frameBorder=\"0\" class=\"giphy-embed\" allowFullScreen></iframe></div><p><a href=\"https://giphy.com/gifs/subway-sverige-funny-cute-\"></a></p>"
     },
     {
-        question: "Which vegetable was the first to be grown in space?",
+        question: "Q: Which vegetable was the first to be grown in space?",
         answer: ["Bean", "Potato", "Cabbage", "Onion"],
         correct: "Potato",
         image: "<div style=\"width:30%;height:0;padding-bottom:20%;position:relative;\"><iframe src=\"https://giphy.com/embed/iiZpZq2rpkkvu\" width=\"100%\" height=\"100%\" style=\"position:absolute\" frameBorder=\"0\" class=\"giphy-embed\" allowFullScreen></iframe></div><p><a href=\"https://giphy.com/gifs/potato-gif-iiZpZq2rpkkvu\"></a></p>"
 
     },
     {
-        question: "Which of the following fruits contains the most vitamin C?",
+        question: "Q: Which of the following fruits contains the most vitamin C?",
         answer: ["Orange", "Raspberry", "Kiwi Fruit", "Apple"],
         correct: "Kiwi Fruit",
         image: "<div style=\"width:30%;height:0;padding-bottom:20%;position:relative;\"><iframe src=\"https://giphy.com/embed/TkTdVluUJd864\" width=\"100%\" height=\"100%\" style=\"position:absolute\" frameBorder=\"0\" class=\"giphy-embed\" allowFullScreen></iframe></div><p><a href=\"https://giphy.com/gifs/food-sweet-green-TkTdVluUJd864\"></a></p>"
     },
     {
-        question: "How many seeds does one strawberry have?",
+        question: "Q: How many seeds does one strawberry have?",
         answer: ["Around 200", "Around 300", "Around 150", "Around 50"],
         correct: "Around 200",
         image: "<div style=\"width:30%;height:0;padding-bottom:20%;position:relative;\"><iframe src=\"https://giphy.com/embed/k5dX3BHkIQAqWRnT1H\" width=\"100%\" height=\"100%\" style=\"position:absolute\" frameBorder=\"0\" class=\"giphy-embed\" allowFullScreen></iframe></div><p><a href=\"https://giphy.com/gifs/cute-food-3d-k5dX3BHkIQAqWRnT1H\">via GIPHY</a></p>"
     },
     {
-        question: "Which of the following vegetables has zero fat content?",
+        question: "Q: Which of the following vegetables has zero fat content?",
         answer: ["Onion", "Celery", "Kale", "Carrot"],
         correct: "Carrot",
         image: "<div style=\"width:30%;height:0;padding-bottom:20%;position:relative;\"><iframe src=\"https://giphy.com/embed/3otOKutrPVezw0TwLm\" width=\"100%\" height=\"100%\" style=\"position:absolute\" frameBorder=\"0\" class=\"giphy-embed\" allowFullScreen></iframe></div><p><a href=\"https://giphy.com/gifs/hug-bunny-carrot-3otOKutrPVezw0TwLm\"></a></p>"
 
     },
     {
-        question: "Growing which fruit is the largest food industry in the world?",
+        question: "Q: Growing which fruit is the largest food industry in the world?",
         answer: ["Grape", "Tomato", "Apple", "Banana"],
         correct: "Grape",
         image: "<div style=\"width:30%;height:0;padding-bottom:20%;position:relative;\"><iframe src=\"https://giphy.com/embed/LNHMgJQr8wXLi\" width=\"100%\" height=\"100%\" style=\"position:absolute\" frameBorder=\"0\" class=\"giphy-embed\" allowFullScreen></iframe></div><p><a href=\"https://giphy.com/gifs/foxadhd-cg-kyle-goodrich-LNHMgJQr8wXLi\"></a></p>"
     },
     {
-        question: "Which of the following never go bad?",
+        question: "Q: Which of the following never go bad?",
         answer: ["Rice", "Wine", "Honey", "Chocolate"],
         correct: "Honey",
         image: "<div style=\"width:30%;height:0;padding-bottom:20%;position:relative;\"><iframe src=\"https://giphy.com/embed/kc8PUzwL0rSqk\" width=\"100%\" height=\"100%\" style=\"position:absolute\" frameBorder=\"0\" class=\"giphy-embed\" allowFullScreen></iframe></div><p><a href=\"https://giphy.com/gifs/kc8PUzwL0rSqk\"></a></p>"
     },
     {
-        question: "Which of the following food can be used to make diamonds?",
+        question: "Q: Which of the following food can be used to make diamonds?",
         answer: ["Peanut Butter", "Salt", "Pistachios", "Caviar"],
         correct: "Peanut Butter",
         image: "<div style=\"width:100%;height:0;padding-bottom:56%;position:relative;\"><iframe src=\"https://giphy.com/embed/Bco5Bt4kYnUcHwHdu7\" width=\"100%\" height=\"100%\" style=\"position:absolute\" frameBorder=\"0\" class=\"giphy-embed\" allowFullScreen></iframe></div><p><a href=\"https://giphy.com/gifs/supersimple-nuts-peanut-butter-Bco5Bt4kYnUcHwHdu7\"></a></p>"
@@ -67,26 +67,88 @@ $(document).ready(function () {
     var correctAnswer = triviaGame[questionNumber].correct;
     var correctImage = triviaGame[questionNumber].image;
     var count = 30;
+    var firstPageCheck = false;
+    var secondPageCheck = false;
 
+    function showSecondPage() {
+        $("#check").show();
+        $("#result").show();
+        $("#image").show();
+
+    }
+
+    function hideFirstPage() {
+        $("#questions").empty();
+        $(".choices").children("div").empty();
+        $("#questions").hide();
+        $(".choices").hide();
+
+    }
+
+    function showFirstPage(){
+        $("#questions").show();
+        $(".choices").show();
+
+    }
+
+    function hideSecondePage(){
+        $("#check").hide();
+        $("#result").hide();
+        $("#image").hide();
+
+        
+    }
+
+    //    start the timer
     var counter = setInterval(timer, 1000); //1000 will  run it every 1 second
-   
+    firstPageCheck = true;
+
     // set time countdown for 30 seconds
     function timer() {
         count = count - 1;
-        if (count <= 0) {
+        if (count === 0 && firstPageCheck === true) {
+            count = 20;
+            showSecondPage();
+            secondPageCheck = true;
+            firstPageCheck = false;
+            $("#check").html("Out of time! &#128543;");
+            $("#result").append(correctAnswer);
+            $("#image").append(correctImage);
+            hideFirstPage();
+            unansweredQuestions++;
+
+        }
+        else if (count === 0 && secondPageCheck === true) {
+            count = 30;
+            showFirstPage();
+            hideSecondePage();
+            firstPageCheck = true;
+            secondPageCheck = false;
+            $("#questions").text(triviaGame[questionNumber].question);
+            $(".choices").children("div").each(function (index) {
+                $(this).append(multiChoices[index]);
+                questionNumber++;
+            })
+
+
+
+        }
+        else if (count < 0) {
             clearInterval(counter);
             return;
         }
 
-       $("span").html("Time Remaining: "+count+" Seconds")
+        $("span").html("Time Remaining: " + count + " Seconds")
     }
 
 
     $("#check").hide();
     $("#result").hide();
     $("#image").hide();
-    
-    timer();
+
+
+
+
 
     // display the first question
     $("#questions").text(triviaGame[questionNumber].question);
@@ -98,28 +160,24 @@ $(document).ready(function () {
 
     // when click on a choice
     $(".choices").children("div").on("click", function () {
-        $("#check").show();
-        $("#result").show();
-        $("#image").show();
+        showSecondPage();
         // ** this indicates the element which is clicked on, use .text() in order to get the content of $this
         console.log($(this).text());
         if ($(this).text() === correctAnswer) {
             $("#check").html("Correct! &#128512;");
             $("#result").append(correctAnswer);
+            correctAnswers++;
         }
         else if ($(this).text() != correctAnswer) {
             $("#check").html("Nope! &#128540;");
             $("#result").append(correctAnswer);
+            incorrectAnswers++;
 
         }
         $("#image").append(correctImage);
 
         // **empty first page question and choices element after $this.text()
-        $("#questions").empty();
-        $("#questions").empty();
-        $(".choices").children("div").empty();
-        $("#questions").hide();
-        $(".choices").hide();
+        hideFirstPage();
 
 
 
