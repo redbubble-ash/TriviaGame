ccb797a28f20ecfa656a8d9853638f9c98718a6c
Jing Xie <xiejing418@gmail.com>
2019-04-26 21:56:07 -0700

    update gif embed image with responsive code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -20,45 +20,45 @@ $(document).ready(function () {
         question: "Which vegetable was the first to be grown in space?",
         answer: ["Bean", "Potato", "Cabbage", "Onion"],
         correct: "Potato",
-        image: "<iframe src=\"https://giphy.com/embed/bPShx901m0HHG\" width=\"480\" height=\"428\" frameBorder=\"0\" class=\"giphy-embed\" allowFullScreen></iframe><p><a href=\"https://giphy.com/gifs/potato-dancing-bPShx901m0HHG\">via GIPHY</a></p>"
+        image: "<div style=\"width:30%;height:0;padding-bottom:20%;position:relative;\"><iframe src=\"https://giphy.com/embed/iiZpZq2rpkkvu\" width=\"100%\" height=\"100%\" style=\"position:absolute\" frameBorder=\"0\" class=\"giphy-embed\" allowFullScreen></iframe></div><p><a href=\"https://giphy.com/gifs/potato-gif-iiZpZq2rpkkvu\"></a></p>"
 
     },
     {
         question: "Which of the following fruits contains the most vitamin C?",
         answer: ["Orange", "Raspberry", "Kiwi Fruit", "Apple"],
         correct: "Kiwi Fruit",
-        image: "<iframe src=\"https://giphy.com/embed/G2mohnSO9SUDe\" width=\"480\" height=\"480\" frameBorder=\"0\" class=\"giphy-embed\" allowFullScreen></iframe><p><a href=\"https://giphy.com/gifs/gifnews-artist-on-tumblr-kyle-goodrich-G2mohnSO9SUDe\">via GIPHY</a></p>"
+        image: "<div style=\"width:30%;height:0;padding-bottom:20%;position:relative;\"><iframe src=\"https://giphy.com/embed/TkTdVluUJd864\" width=\"100%\" height=\"100%\" style=\"position:absolute\" frameBorder=\"0\" class=\"giphy-embed\" allowFullScreen></iframe></div><p><a href=\"https://giphy.com/gifs/food-sweet-green-TkTdVluUJd864\"></a></p>"
     },
     {
         question: "How many seeds does one strawberry have?",
         answer: ["Around 200", "Around 300", "Around 150", "Around 50"],
         correct: "Around 200",
-        image: "<iframe src=\"https://giphy.com/embed/k5dX3BHkIQAqWRnT1H\" width=\"480\" height=\"480\" frameBorder=\"0\" class=\"giphy-embed\" allowFullScreen></iframe><p><a href=\"https://giphy.com/gifs/cute-food-3d-k5dX3BHkIQAqWRnT1H\">via GIPHY</a></p>"
+        image: "<div style=\"width:30%;height:0;padding-bottom:20%;position:relative;\"><iframe src=\"https://giphy.com/embed/k5dX3BHkIQAqWRnT1H\" width=\"100%\" height=\"100%\" style=\"position:absolute\" frameBorder=\"0\" class=\"giphy-embed\" allowFullScreen></iframe></div><p><a href=\"https://giphy.com/gifs/cute-food-3d-k5dX3BHkIQAqWRnT1H\">via GIPHY</a></p>"
     },
     {
         question: "Which of the following vegetables has zero fat content?",
         answer: ["Onion", "Celery", "Kale", "Carrot"],
         correct: "Carrot",
-        image: "<iframe src=\"https://giphy.com/embed/3otOKutrPVezw0TwLm\" width=\"480\" height=\"480\" frameBorder=\"0\" class=\"giphy-embed\" allowFullScreen></iframe><p><a href=\"https://giphy.com/gifs/hug-bunny-carrot-3otOKutrPVezw0TwLm\">via GIPHY</a></p>"
+        image: "<div style=\"width:30%;height:0;padding-bottom:20%;position:relative;\"><iframe src=\"https://giphy.com/embed/3otOKutrPVezw0TwLm\" width=\"100%\" height=\"100%\" style=\"position:absolute\" frameBorder=\"0\" class=\"giphy-embed\" allowFullScreen></iframe></div><p><a href=\"https://giphy.com/gifs/hug-bunny-carrot-3otOKutrPVezw0TwLm\"></a></p>"
 
     },
     {
         question: "Growing which fruit is the largest food industry in the world?",
         answer: ["Grape", "Tomato", "Apple", "Banana"],
         correct: "Grape",
-        image: "<iframe src=\"https://giphy.com/embed/LNHMgJQr8wXLi\" width=\"480\" height=\"480\" frameBorder=\"0\" class=\"giphy-embed\" allowFullScreen></iframe><p><a href=\"https://giphy.com/gifs/foxadhd-cg-kyle-goodrich-LNHMgJQr8wXLi\">via GIPHY</a></p>"
+        image: "<div style=\"width:30%;height:0;padding-bottom:20%;position:relative;\"><iframe src=\"https://giphy.com/embed/LNHMgJQr8wXLi\" width=\"100%\" height=\"100%\" style=\"position:absolute\" frameBorder=\"0\" class=\"giphy-embed\" allowFullScreen></iframe></div><p><a href=\"https://giphy.com/gifs/foxadhd-cg-kyle-goodrich-LNHMgJQr8wXLi\"></a></p>"
     },
     {
         question: "Which of the following never go bad?",
         answer: ["Rice", "Wine", "Honey", "Chocolate"],
         correct: "Honey",
-        image: "<iframe src=\"https://giphy.com/embed/kc8PUzwL0rSqk\" width=\"461\" height=\"480\" frameBorder=\"0\" class=\"giphy-embed\" allowFullScreen></iframe><p><a href=\"https://giphy.com/gifs/kc8PUzwL0rSqk\">via GIPHY</a></p>"
+        image: "<div style=\"width:30%;height:0;padding-bottom:20%;position:relative;\"><iframe src=\"https://giphy.com/embed/kc8PUzwL0rSqk\" width=\"100%\" height=\"100%\" style=\"position:absolute\" frameBorder=\"0\" class=\"giphy-embed\" allowFullScreen></iframe></div><p><a href=\"https://giphy.com/gifs/kc8PUzwL0rSqk\"></a></p>"
     },
     {
         question: "Which of the following food can be used to make diamonds?",
         answer: ["Peanut Butter", "Salt", "Pistachios", "Caviar"],
         correct: "Peanut Butter",
-        image: "<iframe src=\"https://giphy.com/embed/4NsdHaUJCxhgA\" width=\"480\" height=\"480\" frameBorder=\"0\" class=\"giphy-embed\" allowFullScreen></iframe><p><a href=\"https://giphy.com/gifs/jif-peanut-butter-dancing-4NsdHaUJCxhgA\">via GIPHY</a></p>"
+        image: "<div style=\"width:100%;height:0;padding-bottom:56%;position:relative;\"><iframe src=\"https://giphy.com/embed/Bco5Bt4kYnUcHwHdu7\" width=\"100%\" height=\"100%\" style=\"position:absolute\" frameBorder=\"0\" class=\"giphy-embed\" allowFullScreen></iframe></div><p><a href=\"https://giphy.com/gifs/supersimple-nuts-peanut-butter-Bco5Bt4kYnUcHwHdu7\"></a></p>"
     }]
 
 
@@ -71,18 +71,20 @@ $(document).ready(function () {
     $("#result").hide();
     $("#image").hide();
 
+    // display the first question
     $("#questions").text(triviaGame[questionNumber].question);
-
+    // assign each choice to four "#choices div" on html
     $(".choices").children("div").each(function (index) {
 
         $(this).append(multiChoices[index]);
     })
 
-
+    // when click on a choice
     $(".choices").children("div").on("click", function () {
         $("#check").show();
         $("#result").show();
         $("#image").show();
+    // ** this indicates the element which is clicked on, use .text() in order to get the content of $this
         console.log ($(this).text());
         if ($(this).text() === correctAnswer) {
             $("#check").html("Correct! &#128512;");
@@ -94,7 +96,10 @@ $(document).ready(function () {
 
         }
         $("#image").append(correctImage);
-     
+
+        // **empty first page question and choices element after $this.text()
+        $("#questions").empty();
+        $("#questions").empty();
         $(".choices").children("div").empty();
         $("#questions").hide();
         $(".choices").hide();
